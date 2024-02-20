9251d44c9df35ddc9dff95f0ffcd4852630b1fc0
Kakiooo <johnnyjarrah@163.com>
2024-02-20 00:57:09 +0000

    Shooting Logic
    Basic Shooting logic is done

---
+++ Platformer 2d URP Sandbox/Assets/Scripts/JohnnyScript/PlayerControl.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerControl : MonoBehaviour
+{
+    private float _xdir;
+    private Rigidbody2D _refPlayerRb;
+    [SerializeField] float _speed,_upThrust, _firePower;
+    private Vector3 _refToMousePosition;
+    public Transform ShootDirection,ShootPoint;
+    public GameObject Bullet;
+    private void Awake()
+    {
+        _refPlayerRb = this.GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        _refToMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) + new Vector3(0, 0, 10);
+        Vector3 _dir = new Vector3(_refToMousePosition.x - ShootDirection.position.x, _refToMousePosition.y - ShootDirection.position.y);
+        ShootDirection.up = _dir;
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Shoot(_dir);         
+        }
+        Jump(_upThrust);
+    }
+    private void FixedUpdate()
+    {
+        HorizontalMove(_speed);
+       
+    }
+
+
+    void HorizontalMove(float speed)
+    {
+        _xdir = Input.GetAxis("Horizontal");
+        _refPlayerRb.velocity = new Vector2(_xdir * speed,_refPlayerRb.velocity.y);
+    }
+
+    void Jump(float upThrust)
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _refPlayerRb.AddForce(new Vector2(0, upThrust),ForceMode2D.Impulse);
+        }       
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        float blastPower = 15;
+        if (collision.CompareTag("BlastBarrel"))
+        {
+            _refPlayerRb.AddForce(new Vector2(0, blastPower), ForceMode2D.Impulse);
+        }
+    }
+
+    void Shoot(Vector3 dir)
+    {
+        GameObject BulletInstance =Instantiate(Bullet, ShootPoint.position, ShootPoint.rotation);
+        //using GameObject BulletInstance to save the instance of object as variabl.(If no, the instantiate object is not asigned as gameobject in game ) 
+        //如果不用变量存储，脚本无法控制新生成游戏物体的组件对其进行编程（类似于Awake中绑定的步骤）
+        BulletInstance.GetComponent<Rigidbody2D>().AddForce(dir*_firePower, ForceMode2D.Impulse);
+    }
+}
