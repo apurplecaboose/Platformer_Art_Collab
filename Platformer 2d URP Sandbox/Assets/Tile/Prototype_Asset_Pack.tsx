774e7151ad8eda7458581e7a7e04b825d6cb1362
UrFriendPepe <1755090774@qq.com>
2024-02-21 19:01:39 +0000

    auto tiled

---
+++ Platformer 2d URP Sandbox/Assets/Tile/Prototype_Asset_Pack.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Prototype_Asset_Pack" tilewidth="16" tileheight="16" tilecount="182" columns="13">
+ <image source="Prototype_Asset_Pack.png" width="208" height="224"/>
+</tileset>
