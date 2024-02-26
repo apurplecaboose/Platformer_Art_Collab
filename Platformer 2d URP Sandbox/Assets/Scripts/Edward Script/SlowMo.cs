0c2e4d672195af8af44f142e36228b776f8fb639
apurplecaboose <38011964+apurplecaboose@users.noreply.github.com>
2024-02-26 22:04:27 +0000

    YAY I THINK THE SLOW MO IS WORKING

--- Platformer 2d URP Sandbox/Assets/Scripts/Edward Script/SlowMo.cs
+++ Platformer 2d URP Sandbox/Assets/Scripts/Edward Script/SlowMo.cs
@@ -6,49 +6,106 @@ public class SlowMo : MonoBehaviour
 {
     float _sigma_DTime = 0;
     float _slowMoTimeScale = 0.2f;
-    public AnimationCurve ReturntoNormalSpeedCurve;
-    public bool Temp;
+    public AnimationCurve ReturntoNormalSpeedCurve; // for inspector use
+    
+    public float SlowMoResourceTime;
+    public bool SlowMoToggle;
     void Update()
     {
+        ///other script
         if(Input.GetKeyDown(KeyCode.N))
         {
-            Temp = !Temp;
+            SlowMoToggle = !SlowMoToggle;
+            if (SlowMoToggle && SlowMoResourceTime >= 0)
+            {
+                //spam punishment
+                float punishmentAmount = 0.5f;
+                SlowMoResourceTime -= punishmentAmount; //public var
+            }
         }
-        NeoTime(Temp);
+        /// ^^^
+
+        if (SlowMoResourceTime <= 0)
+        {
+            SlowMoToggle = false;
+        }
+        if(Time.timeScale == 1)
+        {
+            Time.fixedDeltaTime = 0.02f; // set it to fixed time step 
+            Debug.Log("Time.fixedDeltaTime Reset Catch Case!!!");
+        }
+
+        SlowMoReady(SlowMoToggle);
     }
-
-
-
-    public void NeoTime(bool toggle)
+    void SlowMoReady(bool activated)
     {
-        if(toggle)
+        if(activated)
+        {
+            if (SlowMoResourceTime <= 0) 
+            {
+                activated = false;
+                NeoTime(false);
+                SlowMoToggle = false;
+            }
+            else
+            {
+                SlowMoResourceTime -= Time.unscaledDeltaTime;
+                NeoTime(true);
+            }
+        }
+        if(!activated)
+        {
+            NeoTime(false);
+
+            float maxSlowMoResourceTime = 5.5f;
+            if (SlowMoResourceTime < maxSlowMoResourceTime)
+            {
+                SlowMoResourceTime += 0.5f * Time.unscaledDeltaTime;
+            }
+            else
+            {
+                SlowMoResourceTime = 5; // catch case
+                Debug.Log("SlowMoResourceTime Overflow Catch Case!!!");
+            }
+        }
+    }
+    void NeoTime(bool startingOrStopping)
+    {
+        if(startingOrStopping)
         {
             Time.timeScale = _slowMoTimeScale;
+            Time.fixedDeltaTime = Time.deltaTime * _slowMoTimeScale;
             _sigma_DTime = 0;
         }
         else
         {
             ReturnFromNeoTime();
         }
-    }
-    public void ReturnFromNeoTime()
-    {
-        float UnscaledLerp(float start, float lerpTime, float unscaledDeltaTime)
+        void ReturnFromNeoTime()
         {
-            float lerpPercentage = unscaledDeltaTime / lerpTime;
-            float output = Mathf.Lerp(start, 1, ReturntoNormalSpeedCurve.Evaluate(lerpPercentage));
-            return output;
-        }
+            float returnTime = 1f;
+            if (_sigma_DTime >= returnTime)
+            {
+                Time.timeScale = 1; // just in case not exactly 1
+                Time.fixedDeltaTime = 0.02f; // set it to fixed time step
+                return;
+            }
 
-        _sigma_DTime += Time.unscaledDeltaTime;
-        float returnTime = 1f;
-        Time.timeScale = UnscaledLerp(_slowMoTimeScale, returnTime, _sigma_DTime);
-        if(_sigma_DTime >= returnTime)
-        {
-            Time.timeScale = 1; // just in case not exactly 1
+            _sigma_DTime += Time.unscaledDeltaTime;
+            float dynamicSlowMoTimeScale = UnscaledLerp(_slowMoTimeScale, returnTime, _sigma_DTime);
+            Time.timeScale = dynamicSlowMoTimeScale;
+            Time.fixedDeltaTime = Time.deltaTime * dynamicSlowMoTimeScale;
 
-            //end function
+
+            float UnscaledLerp(float start, float lerpTime, float unscaledDeltaTime)
+            {
+                float lerpPercentage = unscaledDeltaTime / lerpTime;
+                float output = Mathf.Lerp(start, 1, ReturntoNormalSpeedCurve.Evaluate(lerpPercentage));
+                return output;
+            }
         }
     }
+
+
 
 }
