90ab227e71d9b25099ab5dcda5ed07712b2ac9ba
apurplecaboose <38011964+apurplecaboose@users.noreply.github.com>
2024-03-08 15:16:11 +0000

    flicker tuned

--- Platformer 2d URP Sandbox/Assets/Scripts/Edward Script/ShaderRandomRange.cs
+++ Platformer 2d URP Sandbox/Assets/Scripts/Edward Script/ShaderRandomRange.cs
@@ -4,25 +4,38 @@ using UnityEngine;
 
 public class ShaderRandomRange : MonoBehaviour
 {
-    float _timer, _randRefresh;
+    float _timer1, _randRefresh1, _timer2, _randRefresh2;
     Renderer _Renderer;
     void Start()
     {
         _Renderer = GetComponent<Renderer>();
     }
-
+    float distribution;
+    float randnumb;
     void Update()
     {
-        if(_timer >= _randRefresh)
+        if(_timer1 >= _randRefresh1)
         {
-            _Renderer.material.SetFloat("_FlickSpeed_RandomRange", Random.Range(5, 25f));
-            _Renderer.material.SetFloat("_FlickAmount_RandomRange", Random.Range(0.65f, 0.95f));
-            _randRefresh = Random.Range(0.5f, 1f);
-            _timer = 0;
+            randnumb = Random.Range(5, 25f);
+            distribution = randnumb / 10;
+            _Renderer.material.SetFloat("_FlickSpeed_RandomRange", randnumb);
+            _randRefresh1 = Random.Range(0.65f, 1f);
+            _timer1 = 0;
         }
         else
         {
-            _timer += Time.deltaTime;
+            _timer1 += distribution * Time.deltaTime;
+        }
+
+        if (_timer2 >= _randRefresh2)
+        {
+            _Renderer.material.SetFloat("_FlickAmount_RandomRange", Random.Range(0.65f, 0.95f));
+            _randRefresh2 = Random.Range(0.5f, 01.25f);
+            _timer2 = 0;
+        }
+        else
+        {
+            _timer2 += Time.deltaTime;
         }
 
     }
