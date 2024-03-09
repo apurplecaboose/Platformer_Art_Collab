6a7e00f2a60b6959c2417fbe350f97ac176fc62d
UrFriendPepe <1755090774@qq.com>
2024-03-09 20:54:15 +0000

    dsadfsafasdasgpokamnjsiokadsghnjag

--- Platformer 2d URP Sandbox/Assets/Scripts/JohnnyScript/Campfires.cs
+++ Platformer 2d URP Sandbox/Assets/Scripts/JohnnyScript/Campfires.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Windows;
 
 public class Campfires : MonoBehaviour
 {
@@ -9,17 +10,18 @@ public class Campfires : MonoBehaviour
     PlayerControl _refToPlayerControl;
     SlowMo _refToSlowMo;
     Rigidbody2D _p_rb;
-    [SerializeField] bool IsDash;
-    [SerializeField] float dashMultiplier;
+    [SerializeField] bool _isDash;
+    [SerializeField] float _dashMultiplier;
+    [SerializeField] Vector2 _MinandMaxCamfireForce;
 
     // Update is called once per frame
     private void Awake()
     {
-        dashMultiplier = 3f; //J;control dashing power 
+        _dashMultiplier = 3f; //J;control dashing power 
         _refToPlayer = GameObject.FindGameObjectWithTag("Player");
         _p_rb = _refToPlayer.GetComponent<Rigidbody2D>();
-        _refToPlayerControl= _refToPlayer.GetComponent<PlayerControl>();
-        _refToSlowMo= _refToPlayer.GetComponent<SlowMo>();
+        _refToPlayerControl = _refToPlayer.GetComponent<PlayerControl>();
+        _refToSlowMo = _refToPlayer.GetComponent<SlowMo>();
     }
     private void Update()
     {
@@ -28,16 +30,31 @@ public class Campfires : MonoBehaviour
     }
     void CampFiresTp()
     {
-        if (IsDash)
+        if (_isDash)
         {
             _refToSlowMo.SlowMoToggle = false;//J;return normal speed after shoot the campfire
             Vector2 dashDir;
             dashDir = transform.position - _refToPlayer.transform.position;
             dashDir.Normalize();
             float dashdistance = Vector3.Distance(transform.position, _refToPlayer.transform.position);
-            dashdistance = Mathf.Clamp(dashdistance, 8, 10);
-            _p_rb.AddForce(dashDir * dashdistance * dashMultiplier, ForceMode2D.Impulse); ///E: needs to be forcemode impulse
-            IsDash = false;
+            dashdistance = Mathf.Clamp(dashdistance, _MinandMaxCamfireForce.x, _MinandMaxCamfireForce.y);
+            if (Mathf.Sign(_p_rb.velocity.x) == Mathf.Sign(dashDir.x) && Mathf.Sign(_p_rb.velocity.y) == Mathf.Sign(dashDir.y))
+            {
+                //normal state
+                _p_rb.AddForce(dashDir * dashdistance * _dashMultiplier, ForceMode2D.Impulse); ///E: needs to be forcemode impulse
+                _isDash = false;
+                return; // job well done solider return home!
+            }
+            if (Mathf.Sign(_p_rb.velocity.x) != Mathf.Sign(dashDir.x))
+            {
+                _p_rb.velocity = new Vector2(0, _p_rb.velocity.y); //cancel out x
+            }
+            if (Mathf.Sign(_p_rb.velocity.y) != Mathf.Sign(dashDir.y))
+            {
+                _p_rb.velocity = new Vector2(_p_rb.velocity.x, 0); //cancel out y
+            }
+            _p_rb.AddForce(dashDir * dashdistance * _dashMultiplier, ForceMode2D.Impulse); ///E: needs to be forcemode impulse
+            _isDash = false;
         }
     }
 
@@ -45,7 +62,7 @@ public class Campfires : MonoBehaviour
     {
         if (collision.CompareTag("Bullet"))
         {
-            IsDash = true;
+            _isDash = true;
             Destroy(collision.gameObject);
         }
     }
