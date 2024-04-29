5a92e4ce7f77800c5108b03fe5d232c1e66147b2
VVtx0v0 <2646389288@qq.com>
2024-04-29 19:40:36 +0100

    remove script in prefab and unpacked to remove confilct

--- Platformer 2d URP Sandbox/Assets/Scripts/V Script/UI/DestinationIndicator.cs
+++ Platformer 2d URP Sandbox/Assets/Scripts/V Script/UI/DestinationIndicator.cs
@@ -35,7 +35,7 @@ public class DestinationIndicator : MonoBehaviour
     {
         Debug.Log(_flagCamRelativePos);
         //ArrowRotation();
-        screensize();
+        //screensize();
         IsTargetOnScreen();
         EdgeCheck();
     }
@@ -48,36 +48,36 @@ public class DestinationIndicator : MonoBehaviour
             transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         }
     }
-    void screensize()
-    {
-        _orthoSize = _cam.orthographicSize;
-        float screenWidthOffset = (_orthoSize * (32 / 9))/2;
-        float m =flag.transform.position.y/flag.transform.position.x;
-        Waypoints[0].transform.position = this.transform.position + new Vector3(screenWidthOffset, _orthoSize, 0);//some position;
-        Waypoints[1].transform.position = transform.position+ new Vector3(-screenWidthOffset,_orthoSize,0);
-        //Waypoints[2].transform.position = transform.position+new Vector3(screenWidthOffset,_;
-        //Waypoints[3].transform.position = //some position;
-        //Waypoints[2].transform.position = transform.position+new Vector3(screenWidthOffset)
-        //check up and down
-        if (flag.transform.position.y > transform.position.y + _orthoSize)
-        {
-            Debug.Log("its above me");
-        }
-        else if (flag.transform.position.y < transform.position.y - _orthoSize)
-        {
-            Debug.Log("its below me");
-            //indicator.transform.position = new Vector3(flag.transform.position.x, -_orthoSize+1);
-        }
-        if (flag.transform.position.x > transform.position.x + screenWidthOffset)
-        {
-            Debug.Log("its on my right");
-        }
-        else if (flag.transform.position.y < transform.position.y - screenWidthOffset)
-        {
-            Debug.Log("its on my left");
-        }
+    //void screensize()
+    //{
+    //    _orthoSize = _cam.orthographicSize;
+    //    float screenWidthOffset = (_orthoSize * (32 / 9))/2;
+    //    float m =flag.transform.position.y/flag.transform.position.x;
+    //    Waypoints[0].transform.position = this.transform.position + new Vector3(screenWidthOffset, _orthoSize, 0);//some position;
+    //    Waypoints[1].transform.position = transform.position+ new Vector3(-screenWidthOffset,_orthoSize,0);
+    //    //Waypoints[2].transform.position = transform.position+new Vector3(screenWidthOffset,_;
+    //    //Waypoints[3].transform.position = //some position;
+    //    //Waypoints[2].transform.position = transform.position+new Vector3(screenWidthOffset)
+    //    //check up and down
+    //    if (flag.transform.position.y > transform.position.y + _orthoSize)
+    //    {
+    //        Debug.Log("its above me");
+    //    }
+    //    else if (flag.transform.position.y < transform.position.y - _orthoSize)
+    //    {
+    //        Debug.Log("its below me");
+    //        //indicator.transform.position = new Vector3(flag.transform.position.x, -_orthoSize+1);
+    //    }
+    //    if (flag.transform.position.x > transform.position.x + screenWidthOffset)
+    //    {
+    //        Debug.Log("its on my right");
+    //    }
+    //    else if (flag.transform.position.y < transform.position.y - screenWidthOffset)
+    //    {
+    //        Debug.Log("its on my left");
+    //    }
 
-    }
+    //}
     bool IsTargetOnScreen()
     {
         if (_flagCamRelativePos.x >_camLeftBound&&_flagCamRelativePos.x<_camRightBound)
