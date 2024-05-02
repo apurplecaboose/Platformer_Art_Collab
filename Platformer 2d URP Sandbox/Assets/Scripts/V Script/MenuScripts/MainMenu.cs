7dbb786667bc14688c33117e8b2da745ee6e4127
apurplecaboose <38011964+apurplecaboose@users.noreply.github.com>
2024-05-02 23:09:57 +0100

    secret buttons

--- Platformer 2d URP Sandbox/Assets/Scripts/V Script/MenuScripts/MainMenu.cs
+++ Platformer 2d URP Sandbox/Assets/Scripts/V Script/MenuScripts/MainMenu.cs
@@ -6,12 +6,32 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] SceneEnum.SceneList PlayTargetScene;
-  public void PlayGame()
+    [SerializeField] SceneEnum.SceneList Lv1Scene;
+    [SerializeField] SceneEnum.SceneList Lv2Scene;
+    [SerializeField] SceneEnum.SceneList Lv3Scene;
+    [SerializeField] SceneEnum.SceneList HighScore;
+    public void PlayGame()
     {
         SceneManager.LoadScene(PlayTargetScene.ToString());
     }
     public void QuitGame()
     {
         Application.Quit();
+    }
+    public void Level1()
+    {
+        SceneManager.LoadScene(Lv1Scene.ToString());
+    }
+    public void Level2()
+    {
+        SceneManager.LoadScene(Lv2Scene.ToString());
+    }
+    public void Level3()
+    {
+        SceneManager.LoadScene(Lv3Scene.ToString());
+    }
+    public void SecretHighscore()
+    {
+        SceneManager.LoadScene(HighScore.ToString());
     }
 }
