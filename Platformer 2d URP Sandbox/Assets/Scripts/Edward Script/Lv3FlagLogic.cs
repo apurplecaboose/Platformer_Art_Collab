a45271e9eead1d8673c3956857f46578d81d896b
apurplecaboose <38011964+apurplecaboose@users.noreply.github.com>
2024-05-09 12:19:53 +0100

    vl3 flag and tilecount

---
+++ Platformer 2d URP Sandbox/Assets/Scripts/Edward Script/Lv3FlagLogic.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TransitionsPlus;
+
+public class Lv3FlagLogic : MonoBehaviour
+{
+    [SerializeField] SceneEnum.SceneList _TargetScene;
+    public TransitionProfile TransitionProfile;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            GameManager.P_state = GameManager.PlayerState.Win;//J:limit player movement
+            Time.timeScale = 1;
+            Time.fixedDeltaTime = 0.02f;
+
+            Invoke("TriggerTransition", .5f);
+        }
+    }
+    void TriggerTransition()
+    {
+        TransitionAnimator.Start(TransitionProfile, false, 0, _TargetScene.ToString());
+    }
+}
---
+++ Platformer 2d URP Sandbox/Assets/Scripts/Edward Script/TOOL_TileCountTool.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TOOL_TileCountTool : MonoBehaviour
+{
+    Tilemap tilemap;
+    public Sprite CountingSprite;
+    void Start()
+    {
+        tilemap = GetComponent<Tilemap>();
+        GetTileAmountSprite(CountingSprite);
+    }
+    public int GetTileAmountSprite(Sprite targetSprite)
+    {
+        int amount = 0;
+
+        // loop through all of the tiles        
+        BoundsInt bounds = tilemap.cellBounds;
+        foreach (Vector3Int pos in bounds.allPositionsWithin)
+        {
+            Tile tile = tilemap.GetTile<Tile>(pos);
+            if (tile != null)
+            {
+                if (tile.sprite == targetSprite)
+                {
+                    amount += 1;
+                }
+            }
+        }
+
+        Debug.Log("Total Lv3 Playable Tiles = " + amount);
+        return amount;
+    }
+}
